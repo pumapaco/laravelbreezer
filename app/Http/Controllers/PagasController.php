1d75d5071876f10d46c413596f231fd222a85a93
bryanzt139008 <bryanbozt139008@gmail.com>
2023-11-30 09:59:06 -0500

    29

---
+++ app/Http/Controllers/PagasController.php
@@ -0,0 +1,11 @@
+   <?php
+   namespace  App\Modelos\estudiantes;
+    
+    class PagasController extends Controller
+    {
+        public function fnIndex(){
+            return view ('');
+        }
+
+
+    }
